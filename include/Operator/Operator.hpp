c205baf37c84a165fd589f45e5c6a4e9cec9c622
Josh Presley <joshua.m.presley@icloud.com>
2022-04-06 13:18:05 -0400

    start linked list implementation

--- include/Operator/Operator.hpp
+++ include/Operator/Operator.hpp
@@ -1,14 +1,54 @@
-class Schedule{
-    private:
-        int m_numberOfItems; // currentNumber of boats
-        
+//region List area
+template<class L>
+struct node{
+    node<L>* next;
+    L data;
+};
+template<class L>
+class LinkedList{
     public:
+        node<L>* head;
+        node<L>* last;
+
+        LinkedList<L>(){
+            head = NULL;
+            last = NULL;
+        }
+
+        void add(L data){
+            if(!head){
+                //no elements in list
+                head = new node<L>;
+                head->data = data;
+                head->next = NULL;
+                last = head;
+            }
+        }
+};
+//endregion List area
+
+
+class ScheduleItem{
+    private:
+        int m_arrivalHour;
+        int m_arrivalMinute;
+        ScheduleItem * nextItem;
+        ScheduleItem * previousItem;
+    public:
+        ScheduleItem(int arrivalHour, int arrivalMinute){
+            m_arrivalHour = arrivalHour;
+            m_arrivalMinute = arrivalMinute;
+        }
+        //turn the hour values into one int for ease of comparison
+        int GetArrivalTime(){
+            return (m_arrivalHour * 100) + m_arrivalMinute;
+        }
 
 };
 class Operator{
     private: 
         bool m_isBusy;
-        Schedule * m_schedule;
+        ScheduleItem * m_schedule;
     
     public:
         Operator();
