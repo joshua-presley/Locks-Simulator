e50b2506e0381060b67270fbbd15397e28cba376
Josh Presley <joshua.m.presley@icloud.com>
2022-04-11 19:11:53 -0400

    move LinkedList implementation into cpp file from hpp

--- include/Operator/Operator.hpp
+++ include/Operator/Operator.hpp
@@ -12,55 +12,11 @@ class LinkedList{
     public:
         node<L>* head;
         node<L>* last;
-
-        LinkedList<L>(){
-            head = NULL;
-            last = NULL;
-        }
-
-        void add(L data){
-            if(!head){
-                //no elements in list
-                head = new node<L>;
-                head->data = data;
-                head->next = NULL;
-                last = head;
-            }
-            else{
-                if(last == head){
-                    //one element
-                    last = new node<L>;
-                    last->data = data;
-                    last->next = NULL;
-                    head->next = last;
-                }
-                else{
-                    //more than one element
-                    node<L>* insdata = new node<L>;
-                    insdata->data = data;
-                    insdata->next = NULL;
-                    last->next = insdata;
-                    last = insdata;
-                }
-            }
-        }
-
-        L get(int index){
-            if(index == 0){
-                return this->head->data;
-            }
-            else{
-                node<L>* curr = this->head;
-                for(int i = 0; i<index; ++i){
-                    curr = curr->next;
-                }
-                return curr->data;
-            }
-        }
-
-        L operator[] (int index){
-            return get(index);
-        }
+        
+        LinkedList<L>();
+        void add(L data);
+        L get(int index);
+        L operator[] (int index);
 };
 //endregion List area
 
@@ -92,5 +48,6 @@ class Operator{
         void CloseLock();
         void MoveLock(int Direction);
         void AddToSchedule(ScheduleItem newItem);
+        
 };
 
